df60c4889c567aade3aa8a1fba104a2568508da8
helia shooshtari <112563365+heliashooshtari@users.noreply.github.com>
2022-11-10 18:33:21 +0330

    Add files via upload

---
+++ tamrin1.py
@@ -0,0 +1,38 @@
+#AP homework helia shooshtari
+#tamrin1:
+a=int(input('please enter a number: '))
+b=int(input('please enter a number: '))
+c=int(input('please enter a number: '))
+x=-c/b
+d=(b**2)-(4*a*c)
+e1=(-b+(d**0.5))/2*a
+e2=(-b-(d**0.5))/2*a
+
+if a==0:
+    print('it is the standard form of a line and the answer is:')
+    print(x)
+
+  
+if d==0:
+    print('the equation has two answer which are the same')
+    print(e1)
+ 
+if d<0:
+      print('it does have any answer in real numbers' )
+      
+if d>0:
+       print('it has two answers')
+       print(e1)
+       print(e2)
+      
+   
+#eeeeeh     
+  
+        
+        
+
+        
+        
+      
+        
+   
